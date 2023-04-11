5d221e01b405139fff313e400fad680c2f7c7c9c
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-04-12 03:01:05 +0530

    assets

--- components/Snapbox.js
+++ components/Snapbox.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Snapbox() {
   return (
-    <div className="h-full w-full bg-green-300 lg:flex lg:flex-col lg:justify-center shrink-0 snap-start snap-always">
+    <div className="h-full w-full lg:flex lg:flex-col lg:justify-center shrink-0 snap-start snap-always">
       hello
     </div>
   );
