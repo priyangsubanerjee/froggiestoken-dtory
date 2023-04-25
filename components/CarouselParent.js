cd201762d851f09da9419244b1bd8068f4c874b8
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-04-26 00:17:20 +0530

    view model updated

--- components/CarouselParent.js
+++ components/CarouselParent.js
@@ -5,7 +5,7 @@ function CarouselParent({ children, state, setState }) {
   const totalBars = children.length;
   return (
     <div className="h-screen bg-transparent w-screen">
-      <div className="flex lg:block z-10 overflow-x-auto snap-x lg:snap-none snap-start snap-mandatory fixed inset-0 lg:pt-0 scrollContent">
+      <div className="lg:block z-10 overflow-x-auto lg:snap-none fixed inset-0 lg:pt-0 scrollContent">
         {children}
       </div>
       <Controlbars state={state} setState={setState} totalBars={totalBars} />
