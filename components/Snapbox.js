5eec33cf6679a639eb73301f52ed8f32b3b60d2c
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-04-29 14:15:03 +0530

    snapbox-h

--- components/Snapbox.js
+++ components/Snapbox.js
@@ -12,7 +12,7 @@ function Snapbox({ children, id, setState, state }) {
   return (
     <div
       id={id}
-      className="z-0 lg:h-screen lg:mt-20 lg:min-h-screen h-fit w-full shrink-0 snap-start snap-mandatory snap-always lg:flex lg:flex-col lg:justify-center lg:pb-0 relative text-white "
+      className="z-0 lg:h-screen lg:min-h-[800px] h-fit w-full shrink-0 snap-start snap-mandatory snap-always lg:flex lg:flex-col lg:justify-center lg:pb-0 relative text-white "
     >
       {children}
       <div ref={ref} className="absolute left-1/2 top-1/2"></div>
