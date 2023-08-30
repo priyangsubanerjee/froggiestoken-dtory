74da305a698f65b99b05f7073b0fdd4afdff9fb0
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-30 10:38:41 +0530

    x-frame-options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "x-frame-options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
