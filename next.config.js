6f63a44f91baac5a532c0dc34f167d7c21f2ce54
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-08-30 11:12:39 +0530

    headers

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "x-frame-options",
-            value: "SAMEORIGIN",
-          },
+  //   async headers() {
+  //     return [
+  //       {
+  //         source: "/:path*",
+  //         headers: [
+  //           {
+  //             key: "x-frame-options",
+  //             value: "SAMEORIGIN",
+  //           },
 
-          // x-content-type-options
+  //           // x-content-type-options
 
-          {
-            key: "x-content-type-options",
-            value: "nosniff",
-          },
-        ],
-      },
-    ];
-  },
+  //           {
+  //             key: "x-content-type-options",
+  //             value: "nosniff",
+  //           },
+  //         ],
+  //       },
+  //     ];
+  //   },
 };
 
 module.exports = nextConfig;
